ad66a71d4fc4d35ccac489a9ad6e849d8fbcfdf6
Wagner Souza <wagnersza@gmail.com>
2012-02-28 23:29:44 -0300

    criando instalacao de pacotes nova

--- vagrant/manifests/classes/roles/role_nginx.pp
+++
@@ -1,3 +0,0 @@
-class role_nginx {
-	include nginx::common
-}
--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,16 +1,9 @@
-node sandbox inherits basenode {
-	$zone = "sandbox"
-	$puppet_master = "puppet.local.com"
-	#include user::puppet
+node basenode {
 	exec { "route add -net 10.2.0.0 netmask 255.255.0.0 gw 192.168.0.1 eth1":
 	        unless => 'netstat -nrv | grep -e "10.2.0.0.*192.168.0.1"',
 	}
 }
 
-# node puppetmaster inherits sandbox {
-#         
-#     $puppetmaster_passwd = '$1$6FlrakZW$7P7npg/7rbgAt2r8hrJvC0'
-#     $puppetmaster_local_ipbind = '33.33.33.33'
-#     
-#     include role_puppetmaster
-# }
+node nova01 inherits basenode {
+  include role_nova
+}
