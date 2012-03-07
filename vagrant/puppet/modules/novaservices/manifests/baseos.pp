09210974188a6716aafe76a86ccd2e6242cea114
Wagner Souza <wagnersza@gmail.com>
2012-03-07 11:38:00 -0300

    instalando

--- vagrant/puppet/modules/novaservices/manifests/baseos.pp
+++ vagrant/puppet/modules/novaservices/manifests/baseos.pp
@@ -40,11 +40,50 @@ class novaservices::baseos inherits novaservices::packages {
       onlyif => "test ! -f /var/run/mysqlpass.lock",
       require =>  Service["mysql"],
     }
+    
   } 
   
   ## Handle RedHat derivatives
   else {
     if ($operatingsystem == redhat) or ($operatingsystem == centos) or ($operatingsystem == fedora) {
+    
+      # ntp
+      file { "/etc/ntp.conf":
+        ensure => present,
+        owner => root,
+        group => root,
+        content => template("novaservices/ntp.conf.erb"),
+        require => Package["ntp"],
+      }
+
+      service { "ntpd":
+        enable    => true,
+        ensure    => running,
+        require => File["/etc/ntp.conf"],    
+        hasrestart => true,
+      }
+
+      # mysql
+      file { "/etc/my.cnf":
+        ensure => present,
+        owner => root,
+        group => root,
+        content => template("novaservices/my.cnf.erb"),
+        require =>  Package["mysql-server"],
+      }
+
+      service { "mysqld":
+        enable    => true,
+        ensure    => running,
+        require => File["/etc/my.cnf"],
+        hasrestart => true,    
+      }
+
+      exec { "mysql_change_root_passwd":
+        command => "/usr/bin/mysqladmin -u root password 'mygreatsecret'; touch /var/run/mysql_change_root_passwd.lock",
+        onlyif => "test ! -f /var/run/mysql_change_root_passwd.lock",
+        require =>  Service["mysqld"],
+      }
     
     }
   }
--- vagrant/puppet/modules/novaservices/manifests/packages.pp
+++ vagrant/puppet/modules/novaservices/manifests/packages.pp
@@ -72,6 +72,67 @@ class novaservices::packages {
   else {
     if ($operatingsystem == redhat) or ($operatingsystem == centos) or ($operatingsystem == fedora) {
     
+      # # Create source list file
+      file { "/etc/yum.repos.d/gd-openstack.repo":
+        ensure => present,
+        owner => root,
+        group => root,
+        content => template("novaservices/gd-openstack.repo.erb"),
+      }
+      # 
+      # # Update apt whenever the sources file changes
+      # exec { "apt-get_update":
+      #   command => "/usr/bin/apt-get update",
+      #   subscribe => File["/etc/apt/sources.list"],
+      #   refreshonly => true,
+      #   # require => 
+      # }
+      # 
+      # exec { "apt-get_upgrade":
+      #   command => "/usr/bin/apt-get upgrade; touch /var/run/upgrade.lock", 
+      #   onlyif => "test ! -f /var/run/upgrade.lock",
+      #   require =>  Exec["apt-get_update"],
+      # }
+      # 
+      exec { "add_repo_epel":
+        command => "rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm ; touch /var/run/add_repo_epel.lock",
+        onlyif => "test ! -f /var/run/add_repo_epel.lock",
+      }
+
+      # List of packages to be installed
+      $base_packages = [
+        "bridge-utils",
+        "ntp",
+        "mysql-server",
+        "mysql",
+        "openstack-nova-node-full",
+        # "glance",
+        # "rabbitmq-server",
+        # "python-nova",
+        # "nova-common",
+        # "nova-doc",
+        # "nova-api",
+        # "nova-network",
+        # "nova-objectstore",
+        # "nova-scheduler",
+        # "nova-compute",
+        # "nova-volume",
+        "euca2ools",
+        "unzip",
+        "lvm2",
+        "python-novaclient",
+        "MySQL-python",
+        # "iscsitarget",
+        # "iscsitarget-dkms",
+        # "python-greenlet",
+        # "python-mysqldb",        
+      ]
+
+      package { $base_packages:
+        ensure => latest,
+        require =>  [ Exec["add_repo_epel"], File["/etc/yum.repos.d/gd-openstack.repo"] ]
+      }     
+    
     }
   }    
 }
