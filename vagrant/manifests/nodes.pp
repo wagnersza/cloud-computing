e19078aae04b001bbf82559a8d39c922284f57d7
Wagner Souza <wagnersza@gmail.com>
2012-03-16 10:43:14 -0300

    add route no basenode

--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,7 +1,7 @@
 node basenode {
-  # exec { "route add -net 10.0.2.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
-  #         unless => 'netstat -nrv | grep -e "10.0.2.0.*10.10.10.1"',
-  # }
+  exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
+          unless => 'netstat -nrv | grep -e "10.0.2.0.*10.10.10.1"',
+  }
 }
 
 node novaservices inherits basenode {
