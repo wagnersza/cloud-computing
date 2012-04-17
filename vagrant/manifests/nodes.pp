2e4b918441fbbaeab9b714bb3f7506576baeded6
Wagner Souza <wagnersza@gmail.com>
2012-04-17 17:01:13 -0300

    correcting route

--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,6 +1,6 @@
 node basenode {
   exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
-          unless => 'netstat -nrv | grep -e "10.0.2.0.*10.10.10.1"',
+          unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
   }
 }
 
