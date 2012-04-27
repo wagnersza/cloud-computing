9b9a969159dc10a0b2929700ec212f655857b836
Wagner Souza <wagnersza@gmail.com>
2012-04-27 15:29:28 -0300

    change VM tools name

--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,11 +1,11 @@
 node basenode {
-  exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
-          unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
-  }
-
-  exec { "route add -net 192.168.0.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
-          unless => 'netstat -nrv | grep -e "192.168.0.0.*10.10.10.1"',
-  }
+  # exec { "route add -net 10.2.45.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
+  #         unless => 'netstat -nrv | grep -e "10.2.45.0.*10.10.10.1"',
+  # }
+  # 
+  # exec { "route add -net 192.168.0.0 netmask 255.255.255.0 gw 10.10.10.1 eth1":
+  #         unless => 'netstat -nrv | grep -e "192.168.0.0.*10.10.10.1"',
+  # }
 
 }
 
@@ -21,6 +21,6 @@ node devstack inherits basenode {
    include role_devstack
 }
 
-node yum inherits basenode {
+node tools inherits basenode {
    include role_yum
 }
